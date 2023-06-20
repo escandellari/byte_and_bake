cdc431d5eb70fdff7fa07531d1150d021a96a849
escandellari <elisa.scandellari@gmail.com>
2023-06-20 12:14:30 +0100

    Moved the templates folder in correct app, before creating new app for user registration

--- baking_site/baking_site/settings.py
+++ baking_site/baking_site/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "baking_site.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
