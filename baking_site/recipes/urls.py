05b3954eb59a4acbcd1409e18f7c9ce08b6548c6
escandellari <elisa.scandellari@gmail.com>
2023-06-28 13:50:09 +0100

    Added new pages and relative links

--- baking_site/recipes/urls.py
+++ baking_site/recipes/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from django.views.generic import DetailView
 
 from .models import Post
-from .views import category_view, recipes_view, worldwide_view
+from .views import biscuits_view, bread_view, cakes_view, category_view, recipes_view, worldwide_view
 
 urlpatterns = [
     # path("", views.PostList.as_view(), name="post_list"),
     # path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
+    path("", recipes_view, name="recipes"),
+    path("biscuits/<str:name>", biscuits_view, name="biscuits"),
     path("<slug:slug>/", DetailView.as_view(model=Post), name="post_detail"),
     path("category/<str:name>", category_view, name="category"),
-    path("recipes", recipes_view, name="recipes"),
     path("worldwide", worldwide_view, name="worldwide"),
+    path("bread", bread_view, name="bread"),
+    path("cakes", cakes_view, name="cakes"),
 ]
--- baking_site/recipes/views.py
+++ baking_site/recipes/views.py
@@ -5,6 +5,10 @@ from django.views import generic
 from .models import Post
 
 
+def recipes_view(request):
+    return render(request, "recipes/recipes.html")
+
+
 def category_view(request, name):
     post_list = Post.objects.filter(category__name=name)
     return render(request, "recipes/category.html", {"post_list": post_list, "category_name": name})
@@ -14,5 +18,14 @@ def worldwide_view(request):
     return render(request, "recipes/worldwide.html")
 
 
-def recipes_view(request):
-    return render(request, "recipes/recipes.html")
+def bread_view(request):
+    return render(request, "recipes/bread.html")
+
+
+def biscuits_view(request, name):
+    post_list = Post.objects.filter(category__name=name)
+    return render(request, "recipes/biscuits.html", {"post_list": post_list, "category_name": name})
+
+
+def cakes_view(request):
+    return render(request, "recipes/cakes.html")
