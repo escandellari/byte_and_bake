7a0f17ac0562e6ca45b6b1066cd5c1ff3f836337
escandellari <elisa.scandellari@gmail.com>
2023-06-29 16:12:56 +0100

    Moved static things around to clean up

--- baking_site/baking_site/settings.py
+++ baking_site/baking_site/settings.py
@@ -142,11 +142,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 MEDIA_ROOT = STATIC_ROOT / "media"
 MEDIA_URL = "/media/"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+]
 
 # Log all SQL
 # LOGGING = {
