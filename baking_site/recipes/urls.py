e468bab4b5c5ffc387bf5ea4b02a6e09c283d6c2
escandellari <elisa.scandellari@gmail.com>
2023-06-30 12:08:05 +0100

    Refactor and clean up

--- baking_site/recipes/urls.py
+++ baking_site/recipes/urls.py
@@ -2,25 +2,13 @@ from django.urls import path
 from django.views.generic import DetailView
 
 from .models import Post
-from .views import (
-    AddRecipeView,
-    DeleteRecipeView,
-    EditRecipeView,
-    biscuits_view,
-    bread_view,
-    cakes_view,
-    category_view,
-    recipes_view,
-    worldwide_view,
-)
+from .views import AddRecipeView, DeleteRecipeView, EditRecipeView, category_view, recipes_view
 
 urlpatterns = [
-    # path("", views.PostList.as_view(), name="post_list"),
-    # path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
     path("", recipes_view, name="recipes"),
-    path("category/<str:name>", category_view, name="category"),
-    path("<slug:slug>/", DetailView.as_view(model=Post), name="post_detail"),
     path("add_recipe/", AddRecipeView.as_view(), name="add_recipe"),
     path("edit_recipe/<slug:slug>", EditRecipeView.as_view(), name="edit_recipe"),
     path("delete_recipe/<slug:slug>", DeleteRecipeView.as_view(), name="delete_recipe"),
+    path("category/<str:name>", category_view, name="category"),
+    path("<slug:slug>/", DetailView.as_view(model=Post), name="post_detail"),
 ]
--- baking_site/recipes/views.py
+++ baking_site/recipes/views.py
@@ -31,20 +31,3 @@ def recipes_view(request):
 def category_view(request, name):
     post_list = Post.objects.filter(category__name=name)
     return render(request, "recipes/category.html", {"post_list": post_list, "category_name": name})
-
-
-def worldwide_view(request):
-    return render(request, "recipes/worldwide.html")
-
-
-def bread_view(request):
-    return render(request, "recipes/bread.html")
-
-
-def biscuits_view(request, name):
-    post_list = Post.objects.filter(category__name=name)
-    return render(request, "recipes/biscuits.html", {"post_list": post_list, "category_name": name})
-
-
-def cakes_view(request):
-    return render(request, "recipes/cakes.html")
