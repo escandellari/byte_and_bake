36db60aa2759117718e937a0277348161eba6d88
escandellari <elisa.scandellari@gmail.com>
2023-07-02 12:24:32 +0100

    Experiments with images

--- baking_site/recipes/forms.py
+++ baking_site/recipes/forms.py
@@ -3,7 +3,9 @@ from datetime import datetime
 from django import forms
 from django.urls import reverse, reverse_lazy
 
-from .models import Post
+from .models import Category, Post
+
+cat_choices = list(Category.objects.all().values_list("name", "name"))
 
 
 class AddRecipeForm(forms.ModelForm):
@@ -20,6 +22,7 @@ class AddRecipeForm(forms.ModelForm):
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control"}),
             "author": forms.Select(attrs={"class": "form-control", "value": "", "id": "author", "type": "hidden"}),
+            "category": forms.SelectMultiple(choices=cat_choices, attrs={"class": "form-control"}),
             "snippet": forms.TextInput(attrs={"class": "form-control"}),
             "content": forms.Textarea(
                 attrs={
