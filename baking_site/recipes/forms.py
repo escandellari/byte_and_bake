4afc2ced7bd3baeae6f65dcf6041219193c161f4
escandellari <elisa.scandellari@gmail.com>
2023-07-03 16:41:46 +0100

    Refactor, removed comma

--- baking_site/recipes/forms.py
+++ baking_site/recipes/forms.py
@@ -24,11 +24,7 @@ class AddRecipeForm(forms.ModelForm):
             "author": forms.Select(attrs={"class": "form-control", "value": "", "id": "author", "type": "hidden"}),
             "category": forms.SelectMultiple(choices=cat_choices, attrs={"class": "form-control"}),
             "snippet": forms.TextInput(attrs={"class": "form-control"}),
-            "content": forms.Textarea(
-                attrs={
-                    "class": "form-control",
-                }
-            ),
+            "content": forms.Textarea(attrs={"class": "form-control"}),
         }
 
 
