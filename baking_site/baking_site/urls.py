ef0a3a85b3c8d429441116befeba09bae03512e3
escandellari <elisa.scandellari@gmail.com>
2023-07-04 14:38:56 +0100

    Moving templates around to keep the apps tidy

--- baking_site/baking_site/urls.py
+++ baking_site/baking_site/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="recipes/index.html"), name="index"),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("recipes/", include("recipes.urls")),
     path("ricette/", include("ricette.urls")),
     path("converter/", include("converter.urls")),
