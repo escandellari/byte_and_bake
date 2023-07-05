26c8d364378891f75dc2ca1345e6017b95e5191f
escandellari <elisa.scandellari@gmail.com>
2023-07-05 13:30:55 +0100

    Modified the model, views and forms to improve how the data is saved to the DB

--- baking_site/recipes/urls.py
+++ baking_site/recipes/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.views.generic import DetailView
 
-from .models import Post
-from .views import AddRecipeView, DeleteRecipeView, EditRecipeView, category_view, recipes_view
+from .models import Recipe
+from .views import DeleteRecipeView, EditRecipeView, category_view, recipe_add_view, recipes_view
 
 urlpatterns = [
     path("", recipes_view, name="recipes"),
-    path("add_recipe/", AddRecipeView.as_view(), name="add_recipe"),
-    path("edit_recipe/<slug:slug>", EditRecipeView.as_view(), name="edit_recipe"),
-    path("delete_recipe/<slug:slug>", DeleteRecipeView.as_view(), name="delete_recipe"),
     path("category/<str:name>", category_view, name="category"),
-    path("<slug:slug>/", DetailView.as_view(model=Post), name="post_detail"),
+    path("recipe_add/", recipe_add_view, name="recipe_add"),
+    path("recipe_edit/<slug:slug>", EditRecipeView.as_view(), name="recipe_edit"),
+    path("recipe_delete/<slug:slug>", DeleteRecipeView.as_view(), name="recipe_delete"),
+    path("<slug:slug>/", DetailView.as_view(model=Recipe), name="recipe_detail"),
 ]
