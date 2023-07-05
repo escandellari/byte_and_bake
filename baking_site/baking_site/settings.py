06d8ad6409c0dbc2ceab0fcc24e5eb2aca08fa33
escandellari <elisa.scandellari@gmail.com>
2023-07-05 13:51:48 +0100

    Moved the static general files to the main folder

--- baking_site/baking_site/settings.py
+++ baking_site/baking_site/settings.py
@@ -143,8 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
-MEDIA_ROOT = STATIC_ROOT / "media"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_ROOT = BASE_DIR / "static"
+# MEDIA_ROOT = STATIC_ROOT / "media"
 MEDIA_URL = "/media/"
 
 
