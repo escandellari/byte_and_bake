ef47249edc5e8d4cf7b306325f45cb07cd0e4095
escandellari <elisa.scandellari@gmail.com>
2023-07-06 09:00:06 +0100

    Sorted out where the images are supopsed to be stored

--- baking_site/baking_site/urls.py
+++ baking_site/baking_site/urls.py
@@ -28,4 +28,7 @@ urlpatterns = [
     path("converter/", include("converter.urls")),
     path("registration/", include("django.contrib.auth.urls")),
     path("registration/", include("registration.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
