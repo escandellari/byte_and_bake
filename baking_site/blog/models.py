895ae39ccb135536c96a700335143134647670a6
escandellari <elisa.scandellari@gmail.com>
2023-07-06 14:53:51 +0100

    Renamed model and added fields

--- baking_site/blog/models.py
+++ baking_site/blog/models.py
@@ -5,11 +5,27 @@ from django.template.defaultfilters import slugify as default_slugify
 from django.urls import reverse
 
 
-# Create your models here.
-class BlogPost(models.Model):
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category_name
+
+    def get_absolute_url(self):
+        return reverse("home")
+
+
+class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
+
+    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     slug = models.SlugField(max_length=200, unique=True)
+    category = models.CharField(max_length=255, default="baking")
+
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
     snippet = models.CharField(max_length=150)
@@ -27,7 +43,7 @@ class BlogPost(models.Model):
     def save(self, *args, **kwargs):
         if not self.pk:
             self.slug = self.slugify(self.title)
-        return super(BlogPost, self).save(*args, **kwargs)
+        return super(Post, self).save(*args, **kwargs)
 
     def slugify(self, tag):
         slug = default_slugify(tag)
