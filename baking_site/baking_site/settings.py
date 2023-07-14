9c271c600a2de243adf7778b025452298fb1d986
escandellari <elisa.scandellari@gmail.com>
2023-07-14 11:49:48 +0100

    Render needs static_root

--- baking_site/baking_site/settings.py
+++ baking_site/baking_site/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Path where media is stored
